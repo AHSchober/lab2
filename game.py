c7ff37b62b20ddf27557e0e81951d4869fc59eb0
Austin Schober <a.schober07@gmail.com>
2018-02-08 11:45:56 -0500

    screen text

--- game.py
+++ game.py
@@ -17,7 +17,7 @@ def main():
         pygame.display.set_caption('Launchr 1.0')
         launcher = launchR.launcher(0,SCREEN_HEIGHT-20)
         the_rock = rock.Rock(0,SCREEN_HEIGHT-20)
-        the_target = target.Target(random.random()*SCREEN_WIDTH + 100, SCREEN_HEIGHT - 20, TARGET_WIDTH)
+        the_target = target.Target(random.random()*SCREEN_WIDTH/2 + 100, SCREEN_HEIGHT - 20, TARGET_WIDTH)
 
         objs = [launcher, the_rock, the_target]
 
@@ -44,10 +44,10 @@ def main():
                 the_rock.move(1.0/FPS)
                 if(the_rock.y>SCREEN_HEIGHT):
                         the_rock.moveTo(0, SCREEN_HEIGHT-20)
-                        print('You Missed')
+                        displayText('You Missed', screen)
                 if(the_target.hitBy(the_rock)):
                         the_rock.moveTo(0, SCREEN_HEIGHT-20)
-                        print('w00t')
+                        displayText('FUCK YOU ADAM', screen)
                 #3 draw everything
                 draw_world(screen)
                 for obj in objs:
@@ -70,4 +70,14 @@ def draw_world(surf):
 
         surf.blit(textSurfaceObj, textRectObj)
 
+def displayText(str, surf):
+        fontObj = pygame.font.Font('freesansbold.ttf', 32)
+        textSurfaceObj = fontObj.render(str, True, colors.BLACK)
+        textRectObj = textSurfaceObj.get_rect()
+        textRectObj.center = (SCREEN_WIDTH/2,250)
+        surf.blit(textSurfaceObj, textRectObj)
+
+        pygame.display.update()
+        time.sleep(1)
+        
 main()        
